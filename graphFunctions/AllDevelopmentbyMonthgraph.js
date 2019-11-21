d01836664b2c0e291961916db2ffa38ffcd54627
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2019-11-22 09:21:55 +1300

    Update AllDevelopmentbyMonthgraph.js

--- graphFunctions/AllDevelopmentbyMonthgraph.js
+++ graphFunctions/AllDevelopmentbyMonthgraph.js
@@ -304,7 +304,7 @@ else if (datatypetouse_Development_by_Month == "Constructed Dwellings") {
 	;}
 	
 
-if (timeframe_Development_by_Month == "Years") {
+if (timeframe_Development_by_Month == "Months") {
 	layout = {
 		xaxis: {
 			range: ['2018-01-01', '2019-08-30']
