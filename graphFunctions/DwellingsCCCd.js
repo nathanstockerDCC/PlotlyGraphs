990da9564241a3a7e9610a3fef263b53357dcc61
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2019-11-25 13:58:27 +1300

    Update DwellingsCCCd.js

--- graphFunctions/DwellingsCCCd.js
+++ graphFunctions/DwellingsCCCd.js
@@ -49,20 +49,28 @@ else if (ExtraData == 'Projected Dwellings') {RangeVar = [2009,2048];}
 
 if (ExtraData == 'Projected Dwellings') {
 	layout = {
-   	    title: 'Constructed Dwellings',
-	    showlegend: true,
-		legend: {orientation: 'h', y: '-0.25'},
-    	xaxis: {zeroline: false, tickmode: 'linear', tick0: 0, dtick: 5, tickangle: 45, fixedrange: Zooming, range: RangeVar},
-     	yaxis: {zeroline: false, fixedrange: Zooming, rangemode: "tozero"}, 
+		title: 'Constructed Dwellings',
+		showlegend: true,
+		legend: {
+			orientation: 'h',
+			y: '-0.25',
+			x: '0.5',
+			xanchor: 'center'},
+		xaxis: {zeroline: false, tickmode: 'linear', tick0: 0, dtick: 5, tickangle: 45, fixedrange: Zooming, range: RangeVar},
+		yaxis: {zeroline: false, fixedrange: Zooming, rangemode: "tozero"}, 
 		}
 	;}
 else {
 	layout = {
-   	    title: 'Constructed Dwellings',
-	    showlegend: true,
-		legend: {orientation: 'h', y: '-0.25'},
-    	xaxis: {zeroline: false, tickmode: 'linear', tick0: 0, dtick: 1, tickangle: 45, fixedrange: Zooming, range: RangeVar},
-     	yaxis: {zeroline: false, fixedrange: Zooming, rangemode: "tozero"}, 
+		title: 'Constructed Dwellings',
+		showlegend: true,
+		legend: {
+			orientation: 'h',
+			y: '-0.25',
+			x: '0.5',
+			xanchor: 'center'},
+		xaxis: {zeroline: false, tickmode: 'linear', tick0: 0, dtick: 1, tickangle: 45, fixedrange: Zooming, range: RangeVar},
+     		yaxis: {zeroline: false, fixedrange: Zooming, rangemode: "tozero"}, 
 		}	
 	;}	
 		
