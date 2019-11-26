b5ddfd0b32c801d475e21774efceb0c9dc60d308
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2019-11-26 13:35:58 +1300

    Update AllDevelopmentbyMonthgraph.js

--- graphFunctions/AllDevelopmentbyMonthgraph.js
+++ graphFunctions/AllDevelopmentbyMonthgraph.js
@@ -64,6 +64,17 @@ else if (timeframe_Development_by_Month == "Months") {
 	xValues = $.map(chartData, function(data){
 			return data["Citywide All Months"];
 		});
+
+
+	var filteredDates = xValues.filter(function(e) { return e !== "" })
+	var StartDate = new Date(DateMinMax(filteredDates, "max"));
+	StartDate.setDate(StartDate.getDate()-715);
+	var EndDate = new Date(DateMinMax(filteredDates, "max"));
+	EndDate.setDate(EndDate.getDate()+20);
+
+	dtickValue = TimeToMilliseconds(3, "Months");
+	tick0Value = StartDate-TimeToMilliseconds(-2, "Months");
+	tickformatValue = "%b '%y"
 	
 	xValuesRedone = [];
 	
@@ -76,15 +87,7 @@ else if (timeframe_Development_by_Month == "Months") {
 			}
 	}
 
-	var filteredDates = xValues.filter(function(e) { return e !== "" })
-	var StartDate = new Date(DateMinMax(filteredDates, "max"));
-	StartDate.setDate(StartDate.getDate()-715);
-	var EndDate = new Date(DateMinMax(filteredDates, "max"));
-	EndDate.setDate(EndDate.getDate()+20);
-
-	dtickValue = TimeToMilliseconds(3, "Months");
-	tick0Value = StartDate-TimeToMilliseconds(-2, "Months");
-	tickformatValue = "%b '%y"
+	xValues = DatesRedone
 	
 	;}
 else if (timeframe_Development_by_Month == "Years") {
