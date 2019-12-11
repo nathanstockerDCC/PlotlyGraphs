c96f45256c0be3e8e60fa8702902fe44e7776876
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2019-12-12 08:42:05 +1300

    Update AllDevelopmentbyMonthgraph.js

--- graphFunctions/AllDevelopmentbyMonthgraph.js
+++ graphFunctions/AllDevelopmentbyMonthgraph.js
@@ -102,8 +102,8 @@ else if (timeframe_Development_by_Month == "Years") {
 	EndDate.setDate(EndDate.getDate()+300);
 
 	dtickValue = TimeToMilliseconds(2, "Years");
-//	tick0Value = StartDate-TimeToMilliseconds(0.5, "Years");
-	tick0Value = StartDate;
+	tick0Value = StartDate+TimeToMilliseconds(300, "Days");
+//	tick0Value = StartDate;
 	tickformatValue = "%Y"
 
 /*	xValuesRedone = [];
