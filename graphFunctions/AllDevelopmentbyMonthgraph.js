de6cd3675c8d51fb8112c9e7dfd741e98a093027
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2019-12-13 08:27:27 +1300

    Update AllDevelopmentbyMonthgraph.js

--- graphFunctions/AllDevelopmentbyMonthgraph.js
+++ graphFunctions/AllDevelopmentbyMonthgraph.js
@@ -81,8 +81,8 @@ else if (timeframe_Development_by_Month == "Years") {
 
 	if (datatypetouse_Development_by_Month == "Constructed Dwellings")
 		{
-		tick0Value = new Date('Jun 01 2009');	
-		StartDate = new Date('Jun 01 2009');
+		tick0Value = new Date('2009-06-01');	
+		StartDate = new Date('2009-06-01');
 		StartDate.setDate(StartDate.getDate()-300);		
 		dtickValue = TimeToMilliseconds(1, "Years");
 		}
