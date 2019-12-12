d1891e84e5425a77f90a3c46d1b8c02cd9eaa50e
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2019-12-13 08:49:41 +1300

    Update AllDevelopmentbyMonthgraph.js

--- graphFunctions/AllDevelopmentbyMonthgraph.js
+++ graphFunctions/AllDevelopmentbyMonthgraph.js
@@ -60,7 +60,7 @@ if (timeframe_Development_by_Month == "Months") {
 	EndDate.setDate(EndDate.getDate()+40);
 
 	dtickValue = TimeToMilliseconds(3, "Months");
-	tick0Value = DateMinMax(filteredDates, "max");
+	tick0Value = DateMinMax(filteredDates, "max")+16;
 //	tick0Value = new Date(DateMinMax(filteredDates, "max"));
 //	tick0Value.setDate(tick0Value.getDate()-16);
 	tickmodeValue = "linear";
