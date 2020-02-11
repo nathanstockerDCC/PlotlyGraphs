4218c5893e2b14d14ce3a0b29af0b2fa7845b471
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2020-02-12 12:54:37 +1300

    Update PopGraph.js

--- graphFunctions/PopGraph.js
+++ graphFunctions/PopGraph.js
@@ -1,30 +1,85 @@
 function PopulationGraph(chartData){
 
+var DataToUse = $('#datatypedd_PopGraph option:selected').text();
 
-data = [{
-	x: $.map(chartData, function(data){
-		return data["Year"]
-		;}),
-	y: $.map(chartData, function(data){
-		return data["EstimatedPopulation_Total"]
-		;}),
-	name: 'Estimated Population',
-	line: {width: 6},
-	marker: {color: Colour1}		
-	},{
+if (DataToUse == "Total"){
 		
-	x: $.map(chartData, function(data){
-		return data["Year"]
-		;}),
-	y: $.map(chartData, function(data){
-		return data["ProjectedPopulation_Total"]
-		;}),
-	name: 'Projected Population',
-	line: {width: 6},
-	marker: {color: Colour2}
+	data = [{
+		x: $.map(chartData, function(data){
+			return data["Year"]
+			;}),
+		y: $.map(chartData, function(data){
+			return data["EstimatedPopulation_Total"]
+			;}),
+		name: 'Estimated Population',
+		line: {width: 6},
+		marker: {color: Colour1}		
+		},{
+			
+		x: $.map(chartData, function(data){
+			return data["Year"]
+			;}),
+		y: $.map(chartData, function(data){
+			return data["ProjectedPopulation_Total"]
+			;}),
+		name: 'Projected Population',
+		line: {width: 6},
+		marker: {color: Colour2}
 	}];
-
-
+	;}
+	
+else if (DataToUse == "Growth"){
+		
+	data = [{
+		x: $.map(chartData, function(data){
+			return data["Year"]
+			;}),
+		y: $.map(chartData, function(data){
+			return data["EstimatedPopulation_AbsoluteGrowth"]
+			;}),
+		name: 'Estimated Population Growth',
+		line: {width: 6},
+		marker: {color: Colour1}		
+		},{
+			
+		x: $.map(chartData, function(data){
+			return data["Year"]
+			;}),
+		y: $.map(chartData, function(data){
+			return data["ProjectedPopulation_AbsoluteGrowth"]
+			;}),
+		name: 'Projected Population Growth',
+		line: {width: 6},
+		marker: {color: Colour2}
+	}];
+	;}
+	
+else {	
+	data = [{
+		x: $.map(chartData, function(data){
+			return data["Year"]
+			;}),
+		y: $.map(chartData, function(data){
+			return data["EstimatedPopulation_GrowthRate"]
+			;}),
+		name: 'Estimated Growth Rate',
+		line: {width: 6},
+		marker: {color: Colour1}		
+		},{
+			
+		x: $.map(chartData, function(data){
+			return data["Year"]
+			;}),
+		y: $.map(chartData, function(data){
+			return data["ProjectedPopulation_GrowthRate"]
+			;}),
+		name: 'Projected Growth Rate',
+		line: {width: 6},
+		marker: {color: Colour2}
+	}];
+	;}
+	
+	
 layout = {
 	title: 'Population trends',
 	showlegend: true,
@@ -41,4 +96,4 @@ layout = {
 		
 Plotly.newPlot('PopulationGraph', data, layout);
 	
-} 
+;} 
