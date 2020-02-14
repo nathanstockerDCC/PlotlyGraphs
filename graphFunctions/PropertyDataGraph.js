0271f109ebd9bf5be386d2ee7a5f45c27330b7b6
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2020-02-14 14:17:26 +1300

    Update PropertyDataGraph.js

--- graphFunctions/PropertyDataGraph.js
+++ graphFunctions/PropertyDataGraph.js
@@ -1,77 +1,89 @@
-	
 function PropertyDataGraph(chartData){
 	
 	var DataType = $('#PropertySales_DataType option:selected').text();
 	
-	var LeftAxisFormat = '';
+	var yAxisFormat = '';
 	
-	if(DataType == "Number of House Sales"){LeftAxisFormat = '';}
-	else {LeftAxisFormat = '$,s';};
+	if (DataType == "Number of House Sales")
+		{yAxisFormat = '';}
+	else 
+		{yAxisFormat = '$,s';};
 	
 	
  	if (DataType == "Both"){
-	data = [{
+		data = [{
+			x: $.map(chartData, function(data){
+				return data["Quarter"];
+				}),
+			y: $.map(chartData, function(data){
+				return data["Median House Price"];
+				}),
+			type: 'line',
+			name: 'Median House Price',
+			line: {width: 6},
+			marker: {color: Colour1},
+			yaxis: 'y2'		
+		},{
 		x: $.map(chartData, function(data){
-			return data["Quarter"];
-			}),
-		y: $.map(chartData, function(data){
-			return data["Median House Price"];
-			}),
-		type: 'line',
-		name: 'Median House Price',
-		line: {width: 6},
-		marker: {color: Colour1},
-		yaxis: 'y2'		
-	},{
-	x: $.map(chartData, function(data){
-			return data["Quarter"];
-			}),
-		y: $.map(chartData, function(data){
-			return data["Number of House Sales"];
-			}),
-		type: 'bar',
-		name: 'House Sales',
-		marker: {color: Colour3}
-		}];
+				return data["Quarter"];
+				}),
+			y: $.map(chartData, function(data){
+				return data["Number of House Sales"];
+				}),
+			type: 'bar',
+			name: 'House Sales',
+			marker: {color: Colour3}
+			}];
+			
+		layout = {
+			title: "House Sales and Median Price",
+			showlegend: true,
+			legend: {
+				orientation: 'h',
+				y: '-0.25',
+				x: '0.5',
+				xanchor: 'center'},
+			xaxis: {zeroline: false, tickmode: 'linear', tick0: 0, dtick: 4, tickangle: 45, fixedrange: Zooming},
+			yaxis: {zeroline: false, fixedrange: Zooming, rangemode: "tozero", side: 'right',range:[0,1200]},
+			yaxis2: {zeroline: false, fixedrange: Zooming, rangemode: "tozero", side: 'left',tickformat: '$,s', overlaying: 'y', range:[0,600000]}
+			};
 		
-	layout = {
-		title: "House Sales and Median Price",
-		showlegend: true,
-		legend: {
-			orientation: 'h',
-			y: '-0.25',
-			x: '0.5',
-			xanchor: 'center'},
-		xaxis: {zeroline: false, tickmode: 'linear', tick0: 0, dtick: 4, tickangle: 45, fixedrange: Zooming},
-		yaxis: {zeroline: false, fixedrange: Zooming, rangemode: "tozero", side: 'right',range:[0,1200]},
-		yaxis2: {zeroline: false, fixedrange: Zooming, rangemode: "tozero", side: 'left',tickformat: '$,s', overlaying: 'y', range:[0,600000]}
-		};
-	
-	;}
+		;}
 	
 	else {
-	data = [{
-		x: $.map(chartData, function(data){
-			return data["Quarter"];
-			}),
-		y: $.map(chartData, function(data){
-			return data[DataType];
-			}),
-		type: 'line',
-		name: 'Property Sales',
-		line: {width: 6},
-		marker: {color: Colour1}
-		}];
-	layout = {
-		title: DataType,
-		showlegend: false,
-		xaxis: {zeroline: false, tickmode: 'linear', tick0: 0, dtick: 4, tickangle: 45, fixedrange: Zooming},
-		yaxis: {zeroline: false, fixedrange: Zooming, rangemode: "tozero", tickformat: LeftAxisFormat}
-	};
-	;}
-		
+		data = [{
+			x: $.map(chartData, function(data){
+				return data["Quarter"];
+				}),
+			y: $.map(chartData, function(data){
+				return data[DataType];
+				}),
+			type: 'line',
+			name: 'Property Sales',
+			line: {width: 6},
+			marker: {color: Colour1}
+			}];
+		layout = {
+			title: DataType,
+			showlegend: false,
+			xaxis: {
+				zeroline: false,
+				tickmode: 'linear',
+				tick0: 0,
+				dtick: 4,
+				tickangle: 45,
+				fixedrange: Zooming
+				},
+			yaxis: {
+				zeroline: false,
+				fixedrange: Zooming,
+				rangemode: "tozero",
+				tickformat: yAxisFormat
+				}
+		};
+		;}
+			
 
 
 	Plotly.newPlot('PropertyDataInfo', data, layout);
 	}
-
