1f868dd672a181c372168c3029ad5755351e5773
nathanstockerDCC <57606040+nathanstockerDCC@users.noreply.github.com>
2021-10-12 12:05:30 +1300

    Update AllDevelopmentbyMonthgraph.js

--- graphFunctions/AllDevelopmentbyMonthgraph.js
+++ graphFunctions/AllDevelopmentbyMonthgraph.js
@@ -360,7 +360,7 @@ if (datatypetouse_Development_by_Month == "Total dwellings consented") {
 	layout = {
 		title: 'Total dwellings consented',
 		showlegend: false,
-		annotatios: annonations,
+		annotations: annonations,
 		xaxis: {
 			rangeslider: {visible: rangeSliderVis},
 			rangeselector: selectorOptions,
@@ -464,7 +464,7 @@ else if (datatypetouse_Development_by_Month == "Consented dwellings by type") {
 	layout = {
 		title: 'Consented dwellings by type',
 		showlegend: true,
-		annotatios: annonations,
+		annotations: annonations,
 		legend: {
 			orientation: 'h',
 			y: '-0.25',
@@ -582,7 +582,7 @@ else if (datatypetouse_Development_by_Month == "Value of consented construction"
 	layout = {
 		title: 'Value of consented construction',
 		showlegend: true,
-		annotatios: annonations,
+		annotations: annonations,
 		legend: {
 			orientation: 'h',
 			y: '-0.25',
@@ -633,7 +633,7 @@ else if (datatypetouse_Development_by_Month == "Constructed dwellings") {
 	layout = {
 		title: 'Constructed dwellings',
 		showlegend: false,
-		annotatios: annonations,
+		annotations: annonations,
 		xaxis: {
 			rangeslider: {visible: rangeSliderVis},
 			rangeselector: selectorOptions,
@@ -695,7 +695,7 @@ else if (datatypetouse_Development_by_Month == "Consented dwellings by developme
 	layout = {
 		title: 'Consented dwellings by development type',
 		showlegend: true,
-		annotatios: annonations,
+		annotations: annonations,
 		legend: {
 			orientation: 'h',
 			y: '-0.25',
@@ -864,7 +864,7 @@ else if (datatypetouse_Development_by_Month == "Consented dwellings by zone") {
 	layout = {
 		title: 'Consented dwellings by zone',
 		showlegend: true,
-		annotatios: annonations,
+		annotations: annonations,
 		legend: {
 			orientation: 'h',
 			y: '-0.25',
